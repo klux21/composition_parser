68a899d7c3fc0cddc441ff5217d057634516647f
Dipl.-Ing. Klaus Lux <klux21@gmx.de>
2026-06-26 00:19:56 +0000

    link corrected

--- composition_test.c
+++ composition_test.c
@@ -1,5 +1,5 @@
 #if 0
-rm -f ./_bench_str2num
+rm -f ./_composition_test
 cc -Wall -s -O3 -o _composition_test -I . -I ../str2num -I ../callback_printf ../str2num/str2num.c ../callback_printf/callback_printf.c  ../callback_printf/sfprintf.c iniparse.c composition_test.c
 ./_composition_test
 exit $?
@@ -19,7 +19,7 @@ exit $?
 *                                                                             *
 * --------------------------------------------------------------------------- *
 *                                                                             *
-*  ORIGIN:        https://github/klux21/composition                           *
+*  ORIGIN:        https://github/klux21/composition_parser                    *
 *                                                                             *
 * --------------------------------------------------------------------------- *
 *                                                                             *
